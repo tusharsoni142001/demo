4f1e90397f951204cb22a2ab8dff9e132a954036
Tushar Soni <tusharsoni142001@gmail.com>
2025-04-15 11:00:56 +0530

    Add multiplication method

--- Hello.java
+++ Hello.java
@@ -5,6 +5,7 @@ public class Hello{
 
         System.out.println("Addition of 10 & 20 is " + addNumbers(10, 20));
         System.out.println("Addition of 10 & 20 is " + subtractNumbers(10, 20));
+        System.out.println("Addition of 10 & 20 is " + MULTIPLICATION(10, 20));
         
     }
 
@@ -17,6 +18,12 @@ public class Hello{
     public static int subtractNumbers(int a, int b)
     {
         int sum = a-b;
+        return sum;
+    }
+
+    public static int MULTIPLICATION(int a, int b)
+    {
+        int sum = a*b;
         return sum;
     }
 }
