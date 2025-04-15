15dbf011ad3c7aad26900aec545c17fd25b43e74
Tushar Soni <tusharsoni142001@gmail.com>
2025-04-15 10:36:11 +0530

    Add addNUmbers method

--- Hello.java
+++ Hello.java
@@ -2,5 +2,14 @@ public class Hello{
     public static void main(String[] args) {
         System.out.println("Hello, World!");
         System.out.println("This is a simple Java program.");
+
+        System.out.println("Addition of 10 & 20 is " + addNumbers(10, 20));
+        
+    }
+
+    public static int addNumbers(int a, int b)
+    {
+        int sum = a+b;
+        return sum;
     }
 }
