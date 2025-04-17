14c66a99487955eceb599096043950728503221b
Tushar Soni <tusharsoni142001@gmail.com>
2025-04-17 12:23:03 +0530

    Add new line in new branch

--- NewFile.java
+++ NewFile.java
@@ -2,6 +2,7 @@ public class NewFile {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
         System.out.println("This is a simple Java program in new Branch.");
+        System.out.println("This is a new line added in the new branch.");
 
         
     }
